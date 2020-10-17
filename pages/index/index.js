41ba77cf3b4d57bdcc9c25d68ad59e68e8478b42
guohaoyuan <2671584689@qq.com>
2020-10-17 14:38:31 +0800

    主界面暂时完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -14,11 +14,32 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        let that=this;
-        serve.getnote((data)=>{
+        let that = this;
+        serve.getnote((data) => {
+            var len = Object.keys(data).length
+            var today = new Date()
+            var thisYear = today.getFullYear()
+            var thisMonth = (today.getMonth() + 1 < 10 ? '0' + (today.getMonth() + 1) : today.getMonth() + 1);
+            var thisDay = (today.getDate() < 10 ? '0' + (today.getDate()) : today.getDate());
+
+            for (let i = 0; i < len; i++) {
+                var opdate = new Date(data[i]["updatetime"])
+                data[i]["year"] = opdate.getFullYear();
+                data[i]["month"] = (opdate.getMonth() + 1 < 10 ? '0' + (opdate.getMonth() + 1) : opdate.getMonth() + 1);
+                data[i]["day"] = (opdate.getDate() < 10 ? '0' + (opdate.getDate()) : opdate.getDate());
+
+
+                if(data[i]["year"]===thisYear && data[i]["month"]===thisMonth&&data[i]["day"]===thisDay){
+                    data[i]["quality"] = "today"
+                }
+                else {
+                    data[i]["quality"] = "none"
+                }
+
+            }
             console.log(data)
             this.setData({
-                items:data
+                items: data
             })
         })
 
@@ -28,7 +49,7 @@ Page({
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function () {
-        this.onLoad()
+        // this.onLoad()
     },
 
     /**
@@ -79,9 +100,9 @@ Page({
             url: "/pages/create/index"
         })
     },
-    onEditItem : function (event) {
+    onEditItem: function (event) {
         wx.navigateTo({
-            url:"/pages/create/index"
+            url: "/pages/create/index"
         })
     }
 })
