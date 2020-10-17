5c39ce3f4b4b01b89fc921695f7989ff13bfbb88
guohaoyuan <2671584689@qq.com>
2020-10-17 16:50:44 +0800

    create界面完成，能正常调用添加note

--- pages/index/index.js
+++ pages/index/index.js
@@ -49,14 +49,13 @@ Page({
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function () {
-        // this.onLoad()
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-
+        this.onLoad()
     },
 
     /**
--- utils/myserveAPI.js
+++ utils/myserveAPI.js
@@ -1,19 +1,40 @@
 var GETNOTE = "https://api.zghy.xyz/getnote"
-// var ADDNOTE = "http://zghy.xyz:23333/addnote"
-// var DELNOTE = "http://zghy.xyz:23333/delnote"
-// var EDITNOTE = "http://zghy.xyz:23333/editnote"
+var ADDNOTE = "https://api.zghy.xyz/addnote"
+var DELNOTE = "https://api.zghy.xyz/delnote"
+var EDITNOTE = "https://api.zghy.xyz/editnote"
+
 
 function getnote(callback) {
     wx.request({
         url:GETNOTE,
         data:{},
         method:'GET',
+
         success:function (res) {
             callback(res.data)
         }
     })
 }
 
+function addnote(callback,notedata){
+    console.log(notedata)
+    wx.request({
+        url:ADDNOTE,
+        data:notedata,
+        method:'POST',
+        header: {
+            "Content-Type": "application/x-www-form-urlencoded" //用于post
+        },
+        success:function (res) {
+            callback(res.data)
+        }
+    })
+}
+
+
+
+
 module.exports = {
     getnote:getnote,
+    addnote:addnote
 }
